53353fc5b0b8c2313dad922af83bad2094d091d1
daniel ma <danielma@bubargain.com>
2012-08-18 00:05:39 +0800

    debug limit.py

--- weibo_limit.py
+++ weibo_limit.py
@@ -1,4 +1,7 @@
-mport sys
+#!/usr/bin/python
+#coding:utf-8
+import os
+import sys
 import getopt 
 import webbrowser
 import MySQLdb
@@ -13,7 +16,7 @@ APP_SECRET                 = """36d1bd885bb6553c201b50fc9912b756"""
 CALLBACK_URL               = "http://www.uhquan.com:8888/callback.php"
 
 def do_auth():
-	logging = logging.get_logger('do_auth')
+	logging = Logging.get_logger('do_auth')
 	client = weibo.APIClient(app_key=APP_KEY, app_secret=APP_SECRET,redirect_uri=CALLBACK_URL)
 	code = raw_input ('input code you got:')
 	r = client.request_access_token(code)
@@ -23,6 +26,28 @@ def do_auth():
 	client.set_access_token(access_token,expires_in)
 	print client.get.account__rate_limit_status()
 
+class Logging:
+    func_name = ''
+
+    def __init__(self, func_name):
+        self.func_name = func_name
+
+    @staticmethod
+    def get_logger(func_name):
+        return Logging(func_name.upper())
+
+    @staticmethod
+    def timestamp():
+        return time.strftime('%Y-%m-%d %X', time.localtime(time.time()))
+
+    def info(self, content):
+        print(Logging.timestamp() + "  INFO   [" + self.func_name  + "]: " + content)
+
+    def warning(self, content):
+        print(Logging.timestamp() + " WARNING [" + self.func_name  + "]: " + content)
+
+    def error(self, content):
+        print(Logging.timestamp() + "  ERROR  [" + self.func_name  + "]: " + content)
 
 
 if __name__ == "__main__":
